c01b18516786e04389036f03fb407cf5121186e0
Fernando Medina <ramirof@student.chalmers.se>
2024-11-15 19:03:59 +0100

    Implememtation of dentist UI #5

---
+++ client-ui/src/components/dentist.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="dentist-interface">
+        <h1>Dentist Interface</h1>
+
+    
+    <div class="slot-registration">
+      <h2>Slot Registration</h2>
+      <label for="slot-date">Date:</label>
+      <input type="date" v-model="slotDate" />
+
+      <label for="slot-time">Time:</label>
+      <input type="time" v-model="slotTime" />
+
+      <button @click="registerSlot">Register Slot</button>
+
+      <p v-if="slotMessage" class="message">{{ slotMessage }}</p>
+    </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
