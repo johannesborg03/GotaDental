32dc6ecbb3b0c873d1adce877e2377fb2022c98a
Ravi <ravisha@chalmers.se>
2024-11-15 16:38:31 +0100

    #2 Add Booking model

---
+++ bookingsDB/models/Booking.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var bookingSchema = new mongoose.Schema({
+    booking_id : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    booking_state : {
+        type : Number,
+        required : true,
+        enum: [0, 1] // Only allows values 0 or 1
+    },
+    patient_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Patient',
+    },
+
+    dentist_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Dentist',
+    },
+
+    office_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Office',
+    },
+
+    date_and_time: {
+    type: String
+    }
+
+   
+});
+
+var Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
