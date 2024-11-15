0ded7bc684b81ac6325d0cf251b019be725ff3e0
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-16 00:42:16 +0100

    #3 Creating booking slot for the patient

---
+++ client-ui/src/components/patientUI.vue
@@ -0,0 +1,20 @@
+<template>
+    <template>
+        <div class="patient-interface">
+            <h1>Your account</h1>
+
+            <div class="booking-appointment">
+                <h2>Book a appointment</h2>
+                <label for="dateSlot">Select Date:</label>
+                <input type="date" v-model="bookingDate" />
+
+                <label for="timeSlot">Select Time:</label>
+                <input type="time" v-model="bookingTime" />
+
+                <button @click="bookSlot">Book Dentist Slot</button>
+
+                <p v-if="bookingNotification" class="bookingNotification">{{ bookingNotification }}</p>
+            </div>
+        </div>
+    </template>
+</template>
