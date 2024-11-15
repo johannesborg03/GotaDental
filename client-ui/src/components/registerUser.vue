66f7f693560e1aafea37e3abf6d627a0d5bf460e
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-15 17:41:43 +0100

    Creating the registration form for the user

---
+++ client-ui/src/components/registerUser.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Sign up</h1>
+        <div>
+            <form @submit.prevent="submitForm">
+                <div>
+                    <input type="text" required v-model="username" placeholder="Enter username" class="user-input">
+                </div>
+                <div>
+                    <input type="email" required v-model="email" placeholder="Enter email" class="user-input">
+                </div>
+                <div>
+                    <input type="password" required v-model="password" placeholder="Enter password" class="user-input">
+                </div>
+                <div>
+                    <input type="password" required v-model="ConfirmPassword" placeholder="confirm password"
+                        class="user-input">
+                </div>
+                <div class="terms">
+                    <label>Accept terms and conditions</label>
+                    <input type="checkbox" required v-model="terms">
+                </div>
+                <button type="submit" class="account-button">Sign Up</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+        data() {
+        return {
+            username: '',
+            email: '',
+            password: '',
+            ConfirmPassword: '',
+            terms: false
+        }
+    },
+}
+</script>
