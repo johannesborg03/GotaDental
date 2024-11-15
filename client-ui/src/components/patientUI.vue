35f7bb6d44db32bc588850d6dc37f7157e47f04c
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-16 00:56:19 +0100

    #3 Adding the script for the patient UI

--- client-ui/src/components/patientUI.vue
+++ client-ui/src/components/patientUI.vue
@@ -1,29 +1,43 @@
 <template>
-    <template>
-        <div class="patient-interface">
-            <h1>Your account</h1>
+    <div class="patient-interface">
+        <h1>Your account</h1>
 
-            <div class="booking-appointment">
-                <h2>Book a appointment</h2>
-                <label for="dateSlot">Select Date:</label>
-                <input type="date" v-model="bookingDate" />
+        <div class="booking-appointment">
+            <h2>Book a appointment</h2>
+            <label for="date">Select Date:</label>
+            <input type="date" v-model="bookingDate" />
 
-                <label for="timeSlot">Select Time:</label>
-                <input type="time" v-model="bookingTime" />
+            <label for="time">Select Time:</label>
+            <input type="time" v-model="bookingTime" />
 
-                <button @click="bookSlot">Book Dentist Slot</button>
+            <button @click="bookSlot">Book Dentist Slot</button>
 
-                <p v-if="bookingNotification" class="bookingNotification">{{ bookingNotification }}</p>
-            </div>
-
-            <div class="notifications">
-                <h2>Notification</h2>
-                <ul>
-                    <li v-for="(notification, index) in notifications" :key="index">
-                        {{ notification.message }} - {{ notification.date }}
-                    </li>
-                </ul>
-            </div>
+            <p v-if="bookingNotification" class="bookingNotification">{{ bookingNotification }}</p>
         </div>
-    </template>
+
+        <div class="notifications">
+            <h2>Notification</h2>
+            <ul>
+                <li v-for="(notification, index) in notifications" :key="index">
+                    {{ notification.message }} - {{ notification.date }}
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            bookingDate: "",
+            bookingTime: "",
+            bookingNotification: "",
+
+            notification: [
+                { message: "Your booking for 12:00 PM is booked.", date: "2003-11-16" }
+            ],
+        }
+    }
+}
+</script>
