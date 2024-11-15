ca8d5a506dd0f73cca00a027a99ec1a075deaa46
Fernando Medina <ramirof@student.chalmers.se>
2024-11-15 18:00:52 +0100

    Sample for the login page

---
+++ client-ui/src/components/login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1 class="title">Login</h1>
+        <div class="account-container">
+            <form @submit.prevent="onSubmit">
+                <div>
+                    <input type="text" v-model="username" placeholder="Username" class="user-input" required />
+                </div>
+                <div>
+                    <input type="password" v-model="password" placeholder="Password" class="user-input" required />
+                </div>
+                <button type="submit" class="login-button">Login</button>
+                <div class="register-container">
+                    <p>Don't have an account? </p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+
+        }
+    }
+}
+
+
+
+</script>
+
+<style>
+
+</style>
