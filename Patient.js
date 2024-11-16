4f98ba0d2bd619e7e57ce384fc8d4c7f4b3d8e6d
Johannes Borg <johannes.borg03@gmail.com>
2024-11-16 12:56:22 +0100

    #8 Create a patient file with a template

---
+++ Patient.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var patientSchema = new mongoose.Schema({
+    
+
+}); 
+
+
+var Patient = mongoose.model('Patient', patientSchema);
+module.exports = Patient;
+ 
+
+
+
