165a5b17de1e43bc2eb4149505ce1676d88ec934
Johannes Borg <johannes.borg03@gmail.com>
2024-11-16 13:01:24 +0100

    #8 Add patientId and name fields

--- Patient.js
+++ Patient.js
@@ -2,7 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var patientSchema = new mongoose.Schema({
-    
+    patientId : {
+        type : String,
+        required : true,
+        min : 1,
+        max : 50,
+        unique : true
+    },
+    name : {
+        type : String,
+        required : true,
+        min : 1,
+        max : 50,
+    },
 
 }); 
 
