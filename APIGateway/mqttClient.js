19ce3afff29fec829e1f84512e83b246be4ab3e2
Ravi <ravisha@chalmers.se>
2024-11-18 15:31:26 +0100

    #13 Add connection to mqtt broker

--- APIGateway/mqttClient.js
+++ APIGateway/mqttClient.js
@@ -1,1 +1,17 @@
-git
+const mqtt = require('mqtt');
+const config = require('./config');
+
+const client = mqtt.connect(config.mqttBrokerUrl);
+
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  config.topics.forEach(topic => {
+    client.subscribe(topic, (err) => {
+      if (!err) {
+        console.log(`Subscribed to topic: ${topic}`);
+      }
+    });
+  });
+});
+
+module.exports = client;
