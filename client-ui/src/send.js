8bbfc8ad878f3a6e98d7407a42ff9c6a647e8793
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-18 21:57:03 +0100

    #16 Creating the subscribe file for the RabbitMQ

--- client-ui/src/send.js
+++ client-ui/src/send.js
@@ -1,4 +1,6 @@
 import {connect} from 'amqplib';
+//const amqp = require('amqplib');
+
 
 const connection = await connect('amqp://localhost');
 
@@ -8,4 +10,7 @@ const queue = 'messages';
 const message = 'some info';
 
 await channel.assertQueue(queue, {durable: false});
-channel.sendToQueue(queue, Buffer.from(message));
+//channel.sendToQueue(queue, Buffer.from(message));
+await channel.assertExchange('logs', 'fanout', {durable: false});
+await channel.bindQueue(queue, 'logs', '');
+channel.publish('logs', '', Buffer.from('Hi Mom!'));
