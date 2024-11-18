65f70c1281738320bdf6a45726437acb9deabb74
Johannes Borg <johannes.borg03@gmail.com>
2024-11-18 18:00:50 +0100

    #9 create dentist id field

--- coreServices/userManagementService/src/models/Dentisti.js
+++ coreServices/userManagementService/src/models/Dentisti.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var dentistSchema = new mongoose.schema({
+    dentist_id : {
+        type : String,
+        required : true,
+        min : 1,
+        max : 50,
+        unique : true
+    },
+})
 
 
-
+var Dentist = mongoose.model('Dentist', dentistSchema);
+module.exports = Dentist;
