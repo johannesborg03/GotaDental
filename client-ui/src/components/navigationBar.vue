8b772ebff855c763c71586511898171b9a02bd84
Fernando Medina <ramirof@student.chalmers.se>
2024-11-19 22:06:25 +0100

    #19 implementation of script fo the navigation bar

--- client-ui/src/components/navigationBar.vue
+++ client-ui/src/components/navigationBar.vue
@@ -1,36 +1,30 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">GötaDental</a>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <router-link to="/dentist.vue" class="nav-link">Dentist</router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/patientUI.vue" class="nav-link">Patient</router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/login.vue" class="nav-link">Login</router-link>
-              </li>
-            </ul>
-          </div>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+            <a class="navbar-brand" href="#">GötaDental</a>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/dentist.vue" class="nav-link">Dentist</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/patientUI.vue" class="nav-link">Patient</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/login.vue" class="nav-link">Login</router-link>
+                    </li>
+                </ul>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-      </nav>
+    </nav>
 </template>
 
 <script>
-
+export default {
+    name: "navigationBar",
+};
 </script>
-
