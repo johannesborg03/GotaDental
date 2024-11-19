f239f8cf04d120eb8580d43985f5aae0fb137eb1
Fernando Medina <ramirof@student.chalmers.se>
2024-11-19 22:00:44 +0100

    #19 Implementaion of template for the navigation bar

---
+++ client-ui/src/components/navigationBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">GötaDental</a>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <router-link to="/dentist.vue" class="nav-link">Dentist</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/patientUI.vue" class="nav-link">Patient</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/login.vue" class="nav-link">Login</router-link>
+              </li>
+            </ul>
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+</template>
+
+<script>
+
+</script>
+
