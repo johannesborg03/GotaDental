bce07a825b507a56484095a6a2045d650d1ec069
Ravi Sharma <ravisha@chalmers.se>
2024-11-21 14:19:10 +0100

    #12 Update Notification server port

--- coreServices/NotificationsService/app.js
+++ coreServices/NotificationsService/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 // Variables
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/group10';
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 
 //Require controllers here
