9c164ee1a954b0be007aa8a99253a3994fb2159d
Johannes Borg <johannes.borg03@gmail.com>
2024-11-22 14:41:51 +0100

    #22 Setup notification connection and model template

---
+++ coreServices/NotificationsService/src/models/Notification.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+const bookingDbConnection = require('../utils/dbConnect');
+
+var Schema = mongoose.Schema;
+
+
+
+
+
+
+
+
+
+var Notification = bookingDbConnection.model('Notification', notificationSchema);
+
+module.exports = Notification;
