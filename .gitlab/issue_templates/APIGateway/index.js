915f79d7f25b5a3b42732e43e83a255b33b1a76d
Ravi Sharma <ravisha@chalmers.se>
2024-11-23 16:30:02 +0100

    #13  Delete unnecessary index file

--- .gitlab/issue_templates/APIGateway/index.js
+++
@@ -1,7 +0,0 @@
-const client = require('./mqttClient');
-const { handleMessage } = require('./handlers');
-
-client.on('message', (topic, message) => {
-  console.log(`Received message on ${topic}: ${message.toString()}`);
-  handleMessage(topic, message);
-});
