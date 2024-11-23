171fe0baf49b821e68a1c6452c4bd8a991b630d2
Ravi Sharma <ravisha@chalmers.se>
2024-11-23 16:44:16 +0100

    #13 add Subscribe to a topic and process messages

--- APIGateway/mqttService.js
+++ APIGateway/mqttService.js
@@ -2,9 +2,27 @@ const amqp = require('amqplib');
 
 let channel;
 
-// Connecting to RabbitMQ
+
 async function connectRabbitMQ() {
     const connection = await amqp.connect('amqp://localhost');
     channel = await connection.createChannel();
     console.log('RabbitMQ connected');
 }
+
+async function subscribeToTopic(topic, callback) {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    await channel.assertExchange(topic, 'fanout', { durable: false });
+    const queue = await channel.assertQueue('', { exclusive: true });
+    console.log(`Subscribed to topic "${topic}"`);
+
+    channel.bindQueue(queue.queue, topic, '');
+    channel.consume(queue.queue, (msg) => {
+        const message = JSON.parse(msg.content.toString());
+        console.log(`Received message from topic "${topic}":`, message);
+        callback(message);
+    });
+}
+
+module.exports = { connectRabbitMQ, publishMessage, subscribeToTopic };
