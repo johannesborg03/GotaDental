53d62605a0bfe165967e1cad40653ff7d6f91ef1
Ravi Sharma <ravisha@chalmers.se>
2024-11-23 17:29:53 +0100

    #13 Add script to run server in API Gateway

--- APIGateway/src/routes/timeslot.js
+++ APIGateway/src/routes/timeslot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const timeslotController = require('../controllers/timeslotController');
+const timeslotController = require('../controllers/timeslots');
 
 // Define POST route for timeslot actions
 router.post('/:username', timeslotController.registerSlot);
