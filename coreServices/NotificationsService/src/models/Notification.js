4110cce76c0631a69b4dad9989affca27f3267b3
Johannes Borg <johannes.borg03@gmail.com>
2024-11-23 11:51:34 +0100

    #26 Add patient reference and type attributes

--- coreServices/NotificationsService/src/models/Notification.js
+++ coreServices/NotificationsService/src/models/Notification.js
@@ -3,7 +3,19 @@ const { bookingDbConnection }  = require('../utils/dbConnect');
 
 var Schema = mongoose.Schema;
 
+const notificationSchema = new mongoose.Schema({
+    patient_username: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to User (Patient or Dentist)
+    required: true,
+    },
+type: {
+    type: String,
+    enum: ["booking_confirmation", "booking_cancellation", "reminder", "system_message"],
+    required: true,
+},
 
+});
 
 
 
