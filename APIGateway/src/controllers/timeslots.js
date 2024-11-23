bea628ba1ce81c385e2763aef0cabed28c01388d
Ravi Sharma <ravisha@chalmers.se>
2024-11-23 16:55:52 +0100

    #13 add timeslot controller in apigateway

---
+++ APIGateway/src/controllers/timeslots.js
@@ -0,0 +1,20 @@
+const { publishMessage } = require('../services/mqttService');
+
+async function registerSlot(req, res) {
+  try {
+    const { username } = req.params;
+    const { date, time } = req.body;
+
+    const slotDetails = { username, date, time };
+
+    // Publish to RabbitMQ
+    await publishMessage('timeslot_topic', slotDetails); 
+
+    res.status(200).json({ message: 'Timeslot registered successfully', data: slotDetails });
+  } catch (error) {
+    console.error('Error registering timeslot:', error);
+    res.status(500).json({ message: 'Error registering timeslot' });
+  }
+}
+
+module.exports = { registerSlot };
