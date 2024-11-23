568ca25af268c1df6f862c151139b1c09a3ab2cf
Johannes Borg <johannes.borg03@gmail.com>
2024-11-23 11:55:19 +0100

    #26 Add dentist and appointment attributes

--- coreServices/NotificationsService/src/models/Notification.js
+++ coreServices/NotificationsService/src/models/Notification.js
@@ -1,27 +1,36 @@
 var mongoose = require('mongoose');
-const { bookingDbConnection }  = require('../utils/dbConnect');
+const { bookingDbConnection } = require('../utils/dbConnect');
 
 var Schema = mongoose.Schema;
 
 const notificationSchema = new mongoose.Schema({
     patient_username: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Patient', // Reference to Patient
-    required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient', // Reference to Patient
+        required: false,
     },
-type: {
-    type: String,
-    enum: ["booking_confirmation", "booking_cancellation", "reminder", "system_message"],
-    required: true,
-},
-message: {
-    type: String,
-    required: true,
-},
-timestamp: {
-    type: Date,
-    default: Date.now,
-},
+    dentist_username: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dentist',
+        required: false,
+    },
+    type: {
+        type: String,
+        enum: ["booking_confirmation", "booking_cancellation", "reminder", "system_message"],
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+    appointment_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Appointment',
+    }
 });
 
 
