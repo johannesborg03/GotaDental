452930aa53dffe86f73fb32f6ec5f3ab0b7b65fa
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-24 18:00:00 +0100

    #24 addition of the login logic to the login file

--- client-ui/src/components/login.vue
+++ client-ui/src/components/login.vue
@@ -12,23 +12,47 @@
             </div>
             <button type="submit" class="btn btn-primary w-100">Login</button>
             <div class="mt-3 text-center">
-                <p>Don't have an account? <a href="/register">Register as Dentist</a> or <a href="/register">Register as Patient</a></p>
+                <p>Don't have an account? <a href="/register">Register as Dentist</a> or <a href="/register">Register as
+                        Patient</a></p>
             </div>
         </form>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
             username: '',
             password: '',
-        }
-    }
-}
+        };
+    },
+    methods: {
+        async onSubmit() {
+            try {
+                const response = await axios.post('http://localhost:3000/api/login', {
+                    username: this.username,
+                    password: this.password,
+                });
+
+                localStorage.setItem('token', response.data.token);
+
+                alert('Login successful!');
+                this.$router.push('/patient'); 
+            } catch (err) {
+                console.error('Error during login:', err);
+                if (err.response && err.response.data) {
+                    alert(err.response.data.message || 'Login failed');
+                } else {
+                    alert('An error has occurred. Please try again.');
+                }
+            }
+        },
+    },
+};
 
 
 
 </script>
-
