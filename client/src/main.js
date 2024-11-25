b484c3a474a4c176331a87bc147570fa7cb5bb10
Ravi Sharma <ravisha@chalmers.se>
2024-11-25 16:50:26 +0100

    git commit -m "#31 integrate BootstrapVue Next and update Vite config"

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
+
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 
 
 const app = createApp(App);
