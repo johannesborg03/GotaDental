20dcca567fa762943ef08158c429507562462ee4
Ravi Sharma <ravisha@chalmers.se>
2024-11-25 15:41:50 +0100

    #31 Add boostrap vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -6,6 +7,7 @@ import 'bootstrap';
 
 
 const app = createApp(App);
+app.use(createBootstrap)
 
 app.use(router);
 
