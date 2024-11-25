4ed12e542e83c2b2d4bda29550dfe005d4166c4c
Ravi Sharma <ravisha@chalmers.se>
2024-11-25 14:38:47 +0100

    git commit -m "#31 Fix merge conflict"

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,19 @@
-<script>
-//import navigationBar from './components/navigationBar.vue';
-
-export default {
-  components: {
-    //navigationBar,
-  }
-}
-
-</script>
-
 <template>
-  <div>
-    <nav>
-      
-      <div id="app">
-       
-      </div>
-
-    </nav>
-
+  <div id="app">
+    <Navbar v-if="!$route.meta.hideNavbar" />
     <router-view />
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
+<script>
+import Navbar from './components/Navbar.vue'
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+export default {
+  name: 'App',
+  components: {
+    Navbar 
+  }
 }
+</script>
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
