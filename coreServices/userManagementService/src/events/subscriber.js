d21046f1cdaf58611169687a149daf7bf44bc687
Johannes Borg <johannes.borg03@gmail.com>
2024-11-26 12:07:12 +0100

    #44 Add correlation id to subscribeToTopic

--- coreServices/userManagementService/src/events/subscriber.js
+++ coreServices/userManagementService/src/events/subscriber.js
@@ -10,7 +10,6 @@ async function subscribeToTopic(topic, callback) {
     const channel = await connection.createChannel();
     console.log('Channel created');
 
-
     await channel.assertExchange(topic, 'fanout', { durable: false });
     console.log(`Exchange "${topic}" asserted`);
 
@@ -23,16 +22,9 @@ async function subscribeToTopic(topic, callback) {
     console.log(`Queue "${queue.queue}" bound to topic "${topic}"`);
 
     channel.consume(queue.queue, (msg) => {
-        const rawMessage = msg.content.toString();
-        console.log(`Raw message received: ${rawMessage}`); // Log raw message for debugging
-    
-        try {
-            const message = JSON.parse(rawMessage);
-            console.log(`Received message from topic "${topic}":`, message);
-            callback(message);
-        } catch (error) {
-            console.error('Error processing message:', error);
-        }
+        const message = JSON.parse(msg.content.toString());
+        const { replyTo, correlationId } = msg.properties;
+        callback(message, replyTo, correlationId, channel);
     });
 }
 
