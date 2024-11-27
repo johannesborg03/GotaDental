01c782d7d6c1ab0fb1e0e58ee71da3c9f6acddd5
Ravi Sharma <ravisha@chalmers.se>
2024-11-27 02:40:58 +0100

    #38 Add post note to an appointment

--- APIGateway/src/routes/appointment.js
+++ APIGateway/src/routes/appointment.js
@@ -5,6 +5,7 @@ const appointmentController = require('../controllers/appointments');
 // Route to create a new appointment
 router.post('/api/appointments', appointmentController.createAppointment);
 
-
+// Route to add a note to an appointment
+router.post('/api/appointments/:appointment_id/notes', appointmentController.addNoteToAppointment);
 
 module.exports = router;
