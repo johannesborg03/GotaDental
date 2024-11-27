6d49be55935229ea04dcc1b29a1c50fedc516078
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-27 01:51:57 +0100

    #34 addition of const and if statement to validate input data

--- APIGateway/src/controllers/dentistController.js
+++ APIGateway/src/controllers/dentistController.js
@@ -1,2 +1,13 @@
+const mqtt = require('mqtt');
 const { publishMessage } = require('../mqttService');
 const { v4: uuidv4 } = require('uuid');
+
+exports.registerDentist = async (req, res) => {
+    console.log('Received dentist data:', req.body); 
+    const { ssn, email, name, password } = req.body;
+
+    // Validate the input data
+    if (!ssn || !email || !name || !password) {
+        return res.status(400).json({ message: 'Missing required fields' });
+    }
+}
