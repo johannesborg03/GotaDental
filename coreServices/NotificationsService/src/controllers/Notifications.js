863c586d7c18879e84b71c86321031a5785bba91
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-27 17:44:34 +0100

    #45 Adding logic to the notification controller file in NotificationServices

--- coreServices/NotificationsService/src/controllers/Notifications.js
+++ coreServices/NotificationsService/src/controllers/Notifications.js
@@ -5,7 +5,13 @@ const mongoose = require('mongoose');
 var Notification = require('../models/Notification.js');
 
 
+exports.createNotification = async (req, res) => {
+    const { patient_ssn, dentist_username, type, message, appointment_id } = req.body;
 
+    if (!type) {
+        return res.status(400).json({ message: 'Notification type is required' });
+    }
+};
 
 
 
