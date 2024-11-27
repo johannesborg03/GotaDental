03335c812f3600209f2ade70268c2f902888d54a
Ravi Sharma <ravisha@chalmers.se>
2024-11-27 02:51:53 +0100

    #38 Add route to retrieve all offices

--- APIGateway/src/routes/office.js
+++ APIGateway/src/routes/office.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const officeController = require('../controllers/offices'); // Import the office controller
+const officeController = require('../controllers/offices'); 
 
-
+// Route to retrieve all offices
+router.get('/api/offices', officeController.getAllOffices);
 
 module.exports = router;
