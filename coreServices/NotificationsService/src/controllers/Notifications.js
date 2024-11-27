fc7f1498b53aa1140b51affef3c3d8c592917f18
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-27 17:54:51 +0100

    #45 Addition of the try catch logic

--- coreServices/NotificationsService/src/controllers/Notifications.js
+++ coreServices/NotificationsService/src/controllers/Notifications.js
@@ -11,6 +11,28 @@ exports.createNotification = async (req, res) => {
     if (!type) {
         return res.status(400).json({ message: 'Notification type is required' });
     }
+
+    try {
+        const notification = new Notification({
+            patient_ssn,
+            dentist_username,
+            type,
+            message,
+            appointment_id,
+        });
+
+        const savedNotification = await notification.save();
+        res.status(201).json({
+            message: 'Notification created successfully',
+            notification: savedNotification,
+        });
+    } catch (err) {
+        console.error('Error creating notification:', err.message);
+        res.status(500).json({
+            message: 'Server error while creating notification',
+            error: err.message,
+        });
+    }
 };
 
 
