3b260370e6ac09ad4635236d9538e96de3b0945d
Ravi Sharma <ravisha@chalmers.se>
2024-11-27 02:36:53 +0100

    #38 Add route structure for appointments

---
+++ APIGateway/src/routes/appointment.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const appointmentController = require('../controllers/appointments'); 
+
+router.post();
+
+module.exports = router;
