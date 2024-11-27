92877a3a008520ef6fd1d320c03fc02a4da1666f
Hassan Saleh <hassansa@net.chalmers.se>
2024-11-27 18:43:09 +0100

    #45 addition of the routers in the router file

--- coreServices/NotificationsService/src/apiRoutes/notificationsRoutes.js
+++ coreServices/NotificationsService/src/apiRoutes/notificationsRoutes.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const notificationController = require('../controllers/notificationController'); // Import the controller
+
+// Route to create a new notification
+router.post('/api/notifications', notificationController.createNotification);
+
+// Route to retrieve all notifications for a specific patient
+router.get('/api/notifications/patient/:patient_ssn', notificationController.getNotificationsForPatient);
+
+// Route to retrieve all notifications for a specific dentist
+router.get('/api/notifications/dentist/:dentist_username', notificationController.getNotificationsForDentist);
+
+// Route to retrieve a specific notification
+router.get('/api/notifications/:notification_id', notificationController.getNotificationById);
+
+// Route to delete a specific notification
+router.delete('/api/notifications/:notification_id', notificationController.deleteNotification);
 
 module.exports = router;
