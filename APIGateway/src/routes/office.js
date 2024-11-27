261092704be0f493d6d952ca937a0ee62250817e
Ravi Sharma <ravisha@chalmers.se>
2024-11-27 02:50:29 +0100

    #38 Set up basic router for office related routes

---
+++ APIGateway/src/routes/office.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const officeController = require('../controllers/offices'); // Import the office controller
+
+
+
+module.exports = router;
