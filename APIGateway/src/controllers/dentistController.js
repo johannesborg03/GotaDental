3168ddfaee64a2d136415ca024ca77b12d830e0a
Johannes Borg <johannes.borg03@gmail.com>
2024-11-29 13:53:11 +0100

    #49 Change fields in dentistController

--- APIGateway/src/controllers/dentistController.js
+++ APIGateway/src/controllers/dentistController.js
@@ -4,10 +4,10 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.registerDentist = async (req, res) => {
     console.log('Received dentist data:', req.body);
-    const { ssn, email, name, password } = req.body;
+    const { name, username, email, password } = req.body;
 
     // Validate the input data
-    if (!ssn || !email || !name || !password) {
+    if (!name || !username || !email || !password) {
         return res.status(400).json({ message: 'Missing required fields' });
     }
 
