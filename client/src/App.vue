8c6822e4188861b384f519ead1eb35069dafb289
Johannes Borg <johannes.borg03@gmail.com>
2024-11-29 13:33:46 +0100

    #49 Change some layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,23 +2,29 @@
   <div id="app" class="container">
   <!-- <Navbar v-if="!$route.meta.hideNavbar" /> -->
    <div class="row justify-content-center">
-    <div class="col auto">
+    <div class="col-1">
     <button class="btn btn-primary">
       <router-link to="/registerPatient" class="text-white text-decoration-none">Register Patient</router-link>
     </button>
   </div>
 
-  <div class="col auto">
+
+
+
+  <div class="col-1">
     <button class="btn btn-primary">
       <router-link to="/login" class="text-white text-decoration-none">Login</router-link>
     </button>
   </div>
 
-  <div class="col auto">
+
+  
+  <div class="col-1">
     <button class="btn btn-primary">
       <router-link to="/registerDentist" class="text-white text-decoration-none">Register Dentist</router-link>
     </button>
   </div>
+
 
     <router-view />
   </div>
