cc5f6efabfcff124f6963265300491e4f4cd6c2f
Johannes Borg <johannes.borg03@gmail.com>
2024-11-29 13:21:35 +0100

    #49 Remove navbar from home

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Navbar v-if="!$route.meta.hideNavbar" />
+  <!-- <Navbar v-if="!$route.meta.hideNavbar" /> --> 
     <button class="btn btn-primary">
       <router-link to="/registerPatient" class="text-white text-decoration-none">Register Patient</router-link>
     </button>
