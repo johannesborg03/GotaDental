043ca7e040c8ae989fbf3f5dd3579084025d0613
Johannes Borg <johannes.borg03@gmail.com>
2024-12-01 12:39:37 +0100

    #51 Remove from app to home (login and register buttons)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,5 @@
 <template>
-  <div id="app" class="container">
-  <Navbar v-if="!$route.meta.hideNavbar" /> 
-   <div class="row justify-content-center">
-    <div class="col-1">
-    <button class="btn btn-primary">
-      <router-link to="/registerPatient" class="text-white text-decoration-none">Register Patient</router-link>
-    </button>
-  </div>
-
-
-
-
-  <div class="col-1">
-    <button class="btn btn-primary">
-      <router-link to="/login" class="text-white text-decoration-none">Login</router-link>
-    </button>
-  </div>
-
-
-  
-  <div class="col-1">
-    <button class="btn btn-primary">
-      <router-link to="/registerDentist" class="text-white text-decoration-none">Register Dentist</router-link>
-    </button>
-  </div>
-
-
-    <router-view />
-  </div>
-  </div>
+ 
 </template>
 
 <script>
