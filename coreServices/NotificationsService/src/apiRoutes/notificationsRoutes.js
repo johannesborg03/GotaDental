b34ab5a9936af56b4061d94f476477d33bba3075
Johannes Borg <johannes.borg03@gmail.com>
2024-12-03 10:11:19 +0100

    #56 Comment out unused/unusable logic of notificationRoutes

--- coreServices/NotificationsService/src/apiRoutes/notificationsRoutes.js
+++ coreServices/NotificationsService/src/apiRoutes/notificationsRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const notificationController = require('../controllers/notificationController'); // Import the controller
+const notificationController = require('../controllers/Notifications'); // Import the controller
 
 // Route to create a new notification
-router.post('/api/notifications', notificationController.createNotification);
+//router.post('/api/notifications', notificationController.createNotification);
 
 // Route to retrieve all notifications for a specific patient
-router.get('/api/notifications/patient/:patient_ssn', notificationController.getNotificationsForPatient);
+//router.get('/api/notifications/patient/:patient_ssn', notificationController.getNotificationsForPatient);
 
 // Route to retrieve all notifications for a specific dentist
-router.get('/api/notifications/dentist/:dentist_username', notificationController.getNotificationsForDentist);
+//router.get('/api/notifications/dentist/:dentist_username', notificationController.getNotificationsForDentist);
 
 // Route to retrieve a specific notification
-router.get('/api/notifications/:notification_id', notificationController.getNotificationById);
+//router.get('/api/notifications/:notification_id', notificationController.getNotificationById);
 
 // Route to delete a specific notification
-router.delete('/api/notifications/:notification_id', notificationController.deleteNotification);
+//router.delete('/api/notifications/:notification_id', notificationController.deleteNotification);
 
 module.exports = router;
