15fe78eb5d1cc585bb5863aad3dc44d960a7ae31
Ravi Sharma <ravisha@chalmers.se>
2024-12-04 01:14:57 +0100

    #58 Add descriptive comment for timeslot registration in dentist route (API gateway)

--- APIGateway/src/routes/dentistRoutes.js
+++ APIGateway/src/routes/dentistRoutes.js
@@ -5,6 +5,7 @@ const dentistController = require('../controllers/dentistController'); // Import
 // Route for dentist registration
 router.post('/', dentistController.registerDentist);
 
+// Route for timeslot registration
 router.post('/', dentistController.registerTimeSlot )
 
 module.exports = router;
