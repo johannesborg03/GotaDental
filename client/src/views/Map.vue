43c35bda9ea6e87a35f3ed56f8767c41c1d16931
Johannes Borg <johannes.borg03@gmail.com>
2024-12-05 12:11:22 +0100

    #50 Add map instance and little css

--- client/src/views/Map.vue
+++ client/src/views/Map.vue
@@ -27,13 +27,40 @@ export default {
         this.initMap();
 },
 methods: {
-    
+    initMap(){
+        // Create a Leaflet map instance and attach it to the #map div
+        this.map = L.map("map").setView([51.505, -0.09], 13); // [lat, lng], zoom level
+
+        // Add a tile layer to the map (e.g., OpenStreetMap tiles)
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+
+      // Add a marker to the map
+      L.marker([51.505, -0.09])
+        .addTo(this.map)
+        .bindPopup("A sample marker.")
+        .openPopup();
+    }
 }
 
 }
+
+
 </script>
 
 
 <style>
 /* Add your styles here */
+
+/* Ensure the map container has a height, or the map will not be visible */
+.map-container {
+  width: 100%;
+  height: 100%;
+}
+#map {
+  width: 100%;
+  height: 100%;
+}
 </style>
