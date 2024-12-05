ed775c1aaa0f90c1a18cb695bedfdecee10517a7
Johannes Borg <johannes.borg03@gmail.com>
2024-12-05 11:49:50 +0100

    #50 Add leaflet map from npm install

--- client/src/views/Map.vue
+++ client/src/views/Map.vue
@@ -6,18 +6,32 @@
 
 </template>
 
-
 <script>
+import L from "leaflet"; // Import Leaflet
+
+
 export default {
+    name: "Map",
     data() {
         return {
             
+            map: null, //Reference to leaflet map instance
         };
     },
     async mounted() {
         
 
 
+        this.initMap();
+},
+methods: {
+    
 }
+
 }
 </script>
+
+
+<style>
+/* Add your styles here */
+</style>
