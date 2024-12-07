4fd937e5b842347904ea312a0232e144fc692843
Hassan Saleh <hassansa@net.chalmers.se>
2024-12-07 16:56:40 +0100

    #58 addition of fetchOffices method

--- client/src/views/dentist.vue
+++ client/src/views/dentist.vue
@@ -12,9 +12,9 @@
           <!-- Selection of an office -->
           <div class="mb-3">
             <label for="officeSelect" class="form-label">Select Office:</label>
-            <select id="officeSelect" class="form-select" v-model="officeId" required>
+            <select id="officeSelect" class="form-select" v-model="selectedOffice" required>
               <option value="" disabled>Select an office</option>
-              <option v-for="office in offices" :key="office.office_id" :value="office.office_id">
+              <option v-for="office in offices" :key="office_id" :value="office_id">
                 {{ office.name }}
               </option>
             </select>
@@ -56,7 +56,7 @@ export default {
       slotDate: '',
       slotTime: '',
       username: '',
-      officeId: '',
+      selectedOffice: '',     
       offices: [],
     };
   },
@@ -64,13 +64,7 @@ export default {
   async mounted() {
     this.username = sessionStorage.getItem('userIdentifier');
     console.log(this.username);
-    // Fetching the offices
-    try {
-      const response = await axios.get('http://localhost:4000/api/offices');
-      this.offices = response.data;
-    } catch (error) {
-      console.error('Error fetching offices:', error);
-    }
+    this.fetchOffices();
 },
 
 methods: {
@@ -101,7 +95,7 @@ methods: {
       console.log(this.username);
 
       // Make the POST request to create the time slot 
-      const response = await axios.post('http://localhost:4000/api/timeslots', {
+      const response = await axios.post('http://localhost:4000/api/create', {
         slotDetails
       });
 
@@ -120,7 +114,17 @@ methods: {
       console.error('Error registering slot:', error);
       alert('An error occurred while registering the time slot. Please try again later.');
     }
-  }
+  },
+  async fetchOffices() {
+      try {
+        // Fetch the list of offices from the backend
+        const response = await axios.get("http://localhost:4000/api/offices");
+        this.offices = response.data.offices; // Update the offices array
+      } catch (error) {
+        console.error("Error fetching offices:", error);
+        alert("Failed to load offices. Please try again.");
+      }
+    },
 }
 };
 </script>
