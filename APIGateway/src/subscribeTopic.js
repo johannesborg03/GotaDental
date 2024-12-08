0bfd258a35e1ab198e0256c0b6cd79fea66a7d92
Hassan Saleh <hassansa@net.chalmers.se>
2024-12-08 16:16:18 +0100

    #59 addition of code in the subscribeToTopic file

--- APIGateway/src/subscribeTopic.js
+++ APIGateway/src/subscribeTopic.js
@@ -1,4 +1,5 @@
 const { subscribeToTopic } = require('../mqttService');
+const Timeslot = require('../models/Timeslot'); // Import the Timeslot model
 
 // Callback function that gets executed when a new timeslot is received
 function processTimeslot(message) {
@@ -9,5 +10,13 @@ function processDentistEvent(message) {
     console.log('Processing dentist event:', message);
 }
 
+// Callback function to handle the retrieval of available timeslots
+async function processAvailableTimeslots(message) {
+    try {
+
+    } catch (error) {
+        
+    }
+}
 subscribeToTopic('dentist_topic', processDentistEvent);
 subscribeToTopic('timeslot_topic', processTimeslot);
