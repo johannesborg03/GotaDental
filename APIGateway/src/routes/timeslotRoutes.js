ad47dd033626d9b373aaf868c6a2f7b26ed7f604
Hassan Saleh <hassansa@net.chalmers.se>
2024-12-12 02:45:07 +0100

    #58 re-modeling the office api router

--- APIGateway/src/routes/timeslotRoutes.js
+++ APIGateway/src/routes/timeslotRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { publishMessage } = require('../mqttService'); // Ensure this is correct
 const { v4: uuidv4 } = require('uuid');
+const timeslotController = require('../controllers/timeslotController');
 
 // Create a new timeslot for a specific dentist
 router.post('/:dentist_username', timeslotController.createTimeslot);
