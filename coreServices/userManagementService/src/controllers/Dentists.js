a4319d5c2552c9c748dd26c288b4ec04f4ca534f
Ravi Sharma <ravisha@chalmers.se>
2024-12-13 21:08:19 +0100

    #65 Refactor Dentist controller in API gateway to align with Dentist model

--- coreServices/userManagementService/src/controllers/Dentists.js
+++ coreServices/userManagementService/src/controllers/Dentists.js
@@ -88,6 +88,7 @@ router.put('/api/dentists/:dentist_username', async (req, res) => {
                 password: req.body.password,
                 name: req.body.name,
                 email: req.body.email,
+                date_of_birth: req.body.date_of_birth,
                 appointments : req.body.appointments, //optional
                 timeslots: req.body.timeslots,    // Optional
             },
