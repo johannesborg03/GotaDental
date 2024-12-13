be771a1bd61bb526bd6b4d3f4787bf2afa854741
Ravi Sharma <ravisha@chalmers.se>
2024-12-13 23:32:38 +0100

    #65 Refactor Apigateway register dentist input validation to follow dentist model

--- APIGateway/src/controllers/dentistController.js
+++ APIGateway/src/controllers/dentistController.js
@@ -4,10 +4,10 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.registerDentist = async (req, res) => {
     console.log('Received dentist data:', req.body);
-    const { name, username, email, date_of_birth, password } = req.body;
+    const { name, username, email} = req.body;
 
     // Validate the input data
-    if (!name || !username || !email || !date_of_birth || !password)  {
+    if (!name || !username || !email || !password)  {
         return res.status(400).json({ message: 'Missing required fields' });
     }
 
@@ -15,7 +15,7 @@ exports.registerDentist = async (req, res) => {
     const topic = 'dentists/register';
 
     // Prepare the message for the RabbitMQ broker
-    const dentistData = { name, username, email, date_of_birth, password };
+    const dentistData = { name, username, email, password };
 
     try {
         // Publish the message to the topic (exchange)
