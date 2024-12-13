318d8aaa5f2e9c24eac2403c29544a6e28330bad
Ravi Sharma <ravisha@chalmers.se>
2024-12-13 20:59:08 +0100

    #65 Add test structure for Dentist controller

---
+++ coreServices/userManagementService/tests/controllers/Dentists.test.js
@@ -0,0 +1,11 @@
+import { describe, test, vi, expect } from 'vitest';
+import Dentist from '../../src/models/Dentist';
+
+// Mock the Patient model
+vi.mock('../../src/models/Dentist');
+
+describe ('Patient Model Tests', () => {
+
+
+
+});
