b1c4bbb785de890d7fe6462e487996ab50887148
Ravi Sharma <ravisha@chalmers.se>
2024-12-21 00:33:16 +0100

    #61 Add Appointments page  and its route

--- client/src/router.js
+++ client/src/router.js
@@ -10,6 +10,7 @@ import DentistTimeslot from './views/dentistTimeslot.vue';
 import schedule from './views/Schedule.vue';
 import PatientTimeslot from './views/patientTimeslot.vue';
 import PatientSchedule from './views/PatientSchedule.vue';
+import appointments from './views/appointments.vue';
 
 const routes = [
 
@@ -34,6 +35,8 @@ const routes = [
     { path: '/patientTimeslot', name: 'PatientTimeslot',component: PatientTimeslot},
 
     { path: '/PatientSchedule', name: 'PatientSchedule',component: PatientSchedule},
+
+    {path: '/appointments', name: 'Appointments', component: appointments}
 
 
 ];
