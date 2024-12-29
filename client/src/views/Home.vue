99c2a65200f37086d1c898ba7d668752f519130d
Ravi Sharma <ravisha@chalmers.se>
2024-12-29 18:53:43 +0100

    # Add the login script

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -43,3 +43,56 @@
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            username_ssn: '',
+            password: '',
+        };
+    },
+    methods: {
+        async onSubmit() {
+            console.log("Submitting login form...");
+            console.log("Data being sent:", {
+                username_ssn: this.username_ssn,
+                password: this.password,
+            });
+            try {
+                const response = await axios.post('http://localhost:4000/api/login', {
+                    username_ssn: this.username_ssn,
+                    password: this.password,
+                });
+
+                console.log("Received response:", response.data);
+                sessionStorage.setItem('userIdentifier', this.username_ssn);
+                sessionStorage.setItem('Office', response.data.office);
+                sessionStorage.setItem('OfficeId', response.data.officeId);
+                sessionStorage.setItem('OfficeAddress', response.data.officeAddress);
+        
+                console.log('Office in response:', response.data.office);
+                console.log('OfficeId in response:', response.data.officeId);
+                
+                if (/^\d{12}$/.test(this.username_ssn)) {
+                    // Redirect to dentist homepage if user is a patient (has 12 digits)
+                    this.$router.push('/patient');
+                } else {
+                    // Redirect to patient homepage if user is a dentist
+                    this.$router.push('/dentist');
+                }
+            } catch (err) {
+                console.error('Error during login:', err);
+                if (err.response && err.response.data) {
+                    alert(err.response.data.message || 'Login failed');
+                } else {
+                    alert('An error has occurred. Please try again.');
+                }
+            }
+        },
+    },
+};
+</script>
+
