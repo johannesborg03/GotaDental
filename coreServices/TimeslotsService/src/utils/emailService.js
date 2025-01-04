e6e62cac25a73ca79f020c5a01308ebf98af2576
Johannes Borg <johannes.borg03@gmail.com>
2025-01-04 15:00:57 +0100

    #97 Put env variables into docker container instead of using local .env file

--- coreServices/TimeslotsService/src/utils/emailService.js
+++ coreServices/TimeslotsService/src/utils/emailService.js
@@ -7,14 +7,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT, 10),
-  secure: process.env.SMTP_PORT == 465, // true for port 465, false for other ports
+  secure: process.env.SMTP_PORT == '465', // true for port 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
 
   
-});
 
 
 
